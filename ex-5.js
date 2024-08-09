ab5a29f5b68c0a06c43e7a5cdca7e412942d48ee
ICE K. <info.icejourney@gmail.com>
2024-08-09 16:54:45 +0700

    Update: ex1-8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,14 @@
-//Exercise #5: Shopping and Discount
+// Exercise #5: Shopping and Discount
 
 // Start coding here
+
+// ราคาสินค้าทั้งหมด
+let totalPrice = 27000;
+
+// ส่วนลด 25% คำนวณเป็นจำนวนเงิน
+let discountAmount = totalPrice * 0.25;
+
+// ราคาที่ต้องจ่ายจริงหลังหักส่วนลด
+let finalPrice = totalPrice - discountAmount;
+
+console.log(`ราคาที่ต้องจ่ายจริง = ${finalPrice}`);
--- ex-6.js
+++ ex-6.js
@@ -1,6 +1,11 @@
 // Exercise #6: Displaying message
-
 let totalParts = 13000;
 
-let concatSentence;
-let literalSentence;
+// Start coding here
+let concatSentence = "Total: " + totalParts + " Machine parts"; // การเขียนแบบ String Concatenation
+
+let literalSentence = `Total: ${totalParts} Machine parts`; // Template Literals
+
+
+console.log(concatSentence);
+console.log(literalSentence);
